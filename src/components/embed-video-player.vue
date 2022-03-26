76850d2164909f67c2fa39dc7818b43cf295b575
Pavel Karpovich <pavel.karpovich@outlook.com>
2022-03-27 00:16:51 +0100

    wip: add embed player

---
+++ src/components/embed-video-player.vue
@@ -0,0 +1,28 @@
+<template>
+  <iframe
+    class="fullScreen"
+    :src="videoUrl"
+    frameborder="0"
+    seamless="seamless"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "embed-video-player",
+  props: {
+    videoUrl: String,
+  },
+});
+</script>
+
+<style scoped>
+.fullScreen {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,36 @@
 <template>
-  <q-page class="flex flex-center">Index</q-page>
+  <q-page class="flex">
+    <div class="playerContainer">
+      <embed-video-player :video-url="videoUrl" />
+    </div>
+  </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
+import EmbedVideoPlayer from "../components/embed-video-player";
 import { Config } from "../config";
 
 export default defineComponent({
   name: "IndexPage",
+  components: {
+    EmbedVideoPlayer,
+  },
+  data() {
+    return {
+      videoUrl: Config.mainConferenceVideo,
+    };
+  },
   setup() {
-    console.log(Config.mainConferenceVideo);
     return {};
   },
 });
 </script>
+
+<style scoped>
+.playerContainer {
+  display: flex;
+  flex-grow: 1;
+}
+</style>
