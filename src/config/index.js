bd26a6b2e791a459ff0d36b532fecb243f81009f
Pavel Karpovich <pavel.karpovich@outlook.com>
2022-03-26 23:43:52 +0100

    wip: add config file

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const Config = {
+  mainConferenceVideo: process.env.MAIN_CONFERENCE_VIDEO,
+  liveConferenceVideo: process.env.LIVE_CONFERENCE_VIDEO,
+};
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,7 +5,13 @@
 <script>
 import { defineComponent } from "vue";
 
+import { Config } from "../config";
+
 export default defineComponent({
   name: "IndexPage",
+  setup() {
+    console.log(Config.mainConferenceVideo);
+    return {};
+  },
 });
 </script>
